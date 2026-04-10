160b052ae6680517cc24739a63cf7cd4ace7c1ae
ntcuong668 <ntcuong668@gmail.com>
2026-04-10 12:00:03 +0700

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME='yong-mei-pricing-v4-editable';
-const ASSETS=['./','./index.html?v=4','./index.html','./manifest.webmanifest','./icon.svg','./logo_yong_mei.png'].filter(Boolean);
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE_NAME).then(cache=>cache.put(e.request,c)).catch(()=>{});return r;}).catch(()=>caches.match(e.request).then(c=>c||caches.match('./index.html'))));});
